f12184167766815c4c89b2157431d36d63da82d7
Mateusz Mika <mateuszmikla@wp.pl>
2021-05-28 16:13:19 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import GlobalStyle from "./components/GlobalStyle";
+
+import Home from "./pages/index";
+import AboutMe from "./pages/aboutMe";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path="/aboutme" exact component={AboutMe} />
+          <Route path="/" exact component={Home} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
+
+// export default App;
+
+// import React from "react";
+// import './App.css';
+// import Navbar from './components/Navbar';
+// import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+// import Home from './components/pages/Home'
+// import Services from './components/pages/Services'
+// import Products from './components/pages/Products'
+// import SignUp from './components/pages/SignUp'
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Switch>
+//         <Route path="/" exact component={Home} />
+//         <Route path="/aboutme" exact component={AboutMe} />
+//       </Switch>
+//     </Router>
+//   );
+// }
 
 export default App;
